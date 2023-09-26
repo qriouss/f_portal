66b2ff9c559b825e431f3e5a5b3864ed5ebc70ab
qriouss <117438178+qriouss@users.noreply.github.com>
2023-09-26 14:18:34 +0530

    Update spinner.component.ts

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,12 +1,43 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { LoaderService } from '../loader/loader.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-spinner',
-  templateUrl: './spinner.component.html',
-  styleUrls: ['./spinner.component.css'],
-  encapsulation: ViewEncapsulation.ShadowDom
+  selector: 'app-user-guide',
+  templateUrl: './user-guide.component.html',
+  styleUrls: ['./user-guide.component.css']
 })
-export class SpinnerComponent {
-  constructor(public loader: LoaderService) { }
+
+export class UserGuideComponent {
+
+  ganttData = [
+    { task: 'Task 1', start: new Date(2023, 1, 1), end: new Date(2023, 5, 15), color: 'green' },
+    { task: 'Task 2', start: new Date(2023, 6, 1), end: new Date(2023, 7, 20), color: 'red' },
+    { task: 'Task 3', start: new Date(2023, 6, 1), end: new Date(2023, 10, 20), color: 'grey' },
+    { task: 'Task 4', start: new Date(2023, 4, 1), end: new Date(2023, 7, 20), color: 'purple' },
+    // Add more tasks with their start and end dates
+  ];
+
+  calculateGanttStyle(task: any) {
+    
+    // console.log(startDate,"------",endDate)
+
+    const startMonth = task.start.getMonth() ;
+  const endMonth = task.end.getMonth() ;
+  const gridColumnStart = startMonth;
+  const gridColumnEnd = endMonth + 1;
+
+    // Calculate the percentage of completion based on the current date
+   
+      
+    // Ensure progress is between 0 and 1
+    // const clampedProgress = Math.max(0, Math.min(1, progress));
+    
+    // Calculate the width of the task bar based on progress
+    // const width = progress * 100;
+    // console.log(progress)
+    return {
+      'grid-column-start': gridColumnStart ,
+      'grid-column-end': gridColumnEnd,
+      'background-color': task.color
+    };
+  }
 }

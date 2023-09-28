63eb9db05bf31da0b03e99f5384b55dbf39e000f
qriouss <117438178+qriouss@users.noreply.github.com>
2023-09-28 14:04:50 +0530

    Update spinner.component.ts

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -26,8 +26,8 @@ export class UserGuideComponent implements OnInit{
     
     if (this.selectedEnvironment === '1') {
       this.ganttData = [
-        { task: 'Cloning', start: new Date(2023, 0, 1), end: new Date(2023, 6, 10), color: 'green' },
-        { task: 'Patching', start: new Date(2023, 2, 1), end: new Date(2023, 7, 20), color: 'red' },
+        { task: 'Cloning', start: new Date(2023, 1, 21), end: new Date(2023, 6, 10), color: 'green' },
+        { task: 'Patching', start: new Date(2023, 2, 15), end: new Date(2023, 7, 5), color: 'red' },
         { task: 'Task 3', start: new Date(2023, 2, 1), end: new Date(2023, 3, 20), color: 'grey' },
         { task: 'Task 4', start: new Date(2023, 5, 1), end: new Date(2023, 7, 31), color: 'purple' },
         // Add more tasks with their start and end dates
@@ -37,7 +37,7 @@ export class UserGuideComponent implements OnInit{
         { task: 'Cloning', start: new Date(2023, 0, 1), end: new Date(2023, 4, 10), color: 'green' },
         { task: 'Patching', start: new Date(2023, 6, 1), end: new Date(2023, 7, 20), color: 'red' },
         { task: 'Task 3', start: new Date(2023, 6, 1), end: new Date(2023, 10, 20), color: 'grey' },
-        { task: 'Task 4', start: new Date(2023, 0, 1), end: new Date(2023, 11, 31), color: 'purple' },
+        { task: 'Task 4', start: new Date(2023, 0, 1), end: new Date(2023, 11, 25), color: 'purple' },
         // Add more tasks with their start and end dates
       ];
     }
@@ -48,25 +48,50 @@ export class UserGuideComponent implements OnInit{
 
   
 
-  calculateGanttStyle(task: any) {
+  // calculateGanttStyle(task: any) {
     
-    // console.log(startDate,"------",endDate)
-    const myDate=new Date(2023,0,1);
-    const startMonth = task.start.getMonth()+1 ;
+  //   // console.log(startDate,"------",endDate)
+  //   const myDate=new Date(2023,0,1);
+  //   const startMonth = task.start.getMonth()+1 ;
+  // const endMonth = task.end.getMonth()+1 ;
+  // const gridColumnStart = startMonth;
+  // const gridColumnEnd = endMonth+1 ;
+  // const startDate = ((task.start.getTime()-myDate.getTime())/86400000)+1;
+  // const endDate = ((task.end.getTime()-myDate.getTime())/86400000)+1;
+  // const daysDifference = Math.ceil((endDate - startDate));
+  // // const  actualdiff= (daysDifference/366)*100;
+  //   console.log(startDate)
+  
+  //   return {
+  //     'grid-column-start': gridColumnStart ,
+  //     'grid-column-end': gridColumnEnd ,
+  //     // 'width': `${daysDifference}`,
+  //     'background-color': task.color
+  //   };
+  // }
+
+  calculateGanttStyle(task: any) {
+    const startDate = task.start;
+    const endDate = task.end;
+      const startMonth = task.start.getMonth()+1 ;
   const endMonth = task.end.getMonth()+1 ;
   const gridColumnStart = startMonth;
   const gridColumnEnd = endMonth+1 ;
-  const startDate = ((task.start.getTime()-myDate.getTime())/86400000)+1;
-  const endDate = ((task.end.getTime()-myDate.getTime())/86400000)+1;
-  const daysDifference = Math.ceil((endDate - startDate));
-  // const  actualdiff= (daysDifference/366)*100;
-    console.log(startDate)
+  const myDate=new Date(2023,0,1);
+  const start= Math.ceil((startDate.getTime() - myDate.getTime()) / (1000 * 60 * 60 * 24));
+  console.log(start)
+  
+    // Calculate the width based on the number of days
+    const daysDifference = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24));
+  
   
     return {
-      'grid-column-start': gridColumnStart ,
-      'grid-column-end': gridColumnEnd ,
-      // 'width': `${daysDifference}`,
+      'margin-left': `${(start/30)*100 }%` ,
+      // 'grid-column-end': `${gridColumnEnd }` ,
+      'width': `${((daysDifference/30))*100}%`, // Set the width in pixels based on the date difference
       'background-color': task.color
     };
   }
+
+
 }
